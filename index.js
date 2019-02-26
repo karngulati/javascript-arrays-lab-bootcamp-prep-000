28788e4e8251440c5394c785733e27081db95ad0
Karn Gulati <karngulati@gmail.com>
2019-02-26 17:53:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function prependKitten(array, element) {
 }
 
 function removeLastKitten(array) {
-  kittens = kittens.slice(0, kittens.length - 1)
+  kittens.slice(0, kittens.length - 1)
   return kittens
 }
 
